c72ee1ca63ea4277a47efdf6c60f4615a582455d
JM <limjungmok@gmail.com>
2015-08-13 16:01:01 +0900

    etc

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,13 @@ Rails.application.routes.draw do
   get 'contact' 	=> 'static_pages#contact'
 
   #Facebook
-
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :sessions, only: [:create, :destroy]
   
-  #users controller
+  #users controller(로그인 페이스북만 쓰면 필요없음)
   get 'signup'		=> 'users#new'
 
   resources :users
