37c6293ce9ec16cc4001e0d35eb3686f244b3c0c
JM <limjungmok@gmail.com>
2015-08-15 01:13:51 +0900

    account setting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,16 @@ class SessionsController < ApplicationController
   def create
     user = FacebookUser.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to edit_facebook_user_path(:id => session[:user_id])
+    if user.nickname == nil
+    	redirect_to edit_facebook_user_path(:id => session[:user_id])
+    else
+    	redirect_to root_path
+    end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
-
+  
 end
