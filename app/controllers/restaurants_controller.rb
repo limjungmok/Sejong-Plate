343fab1e3756f7e8a554126ecdec57a88c3ca98f
coco <jws1784@gmail.com>
2015-08-18 11:17:45 +0900

    add multi picture upload

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,34 @@
 class RestaurantsController < ApplicationController
 	def show
+		@restaurant = Restaurant.find(params[:id])
+		@albums = @restaurant.albums.all
 	end
 
 	def index
 	end
+
+	def new
+		@restaurant = Restaurant.new
+		@album = @restaurant.albums.build
+	end
+
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
+		respond_to do |format|
+			if @restaurant.save
+				params[:albums]['picture'].each do |a|
+					@album = @restaurant.albums.create!(:picture => a)
+				end
+				format.html { redirect_to @restaurant, notice: 'Post was successfully create.'}
+			else
+				format.html { render action: 'new'}
+			end
+		end
+	end
+
+	private
+
+		def restaurant_params
+			params.require(:restaurant).permit(:r_name, :r_phone_number, :r_click_count, :r_like, albums_attributes: [:id, :restaurant_id, :picture])
+		end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
+	mount_uploader :picture, PictureUploader
 	belongs_to :restaurant
 end
