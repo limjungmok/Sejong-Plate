2250804ababbabdc05f28833808ca461bde47da6
coco <ysj08240@gmail.com>
2015-08-19 22:19:13 +0900

    index page add update & delete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,9 +18,7 @@ class RestaurantsController < ApplicationController
 		respond_to do |format|
 			if @restaurant.save
 				unless params[:albums].nil?
-					params[:albums]['picture'].each do |a|
-						@album = @restaurant.albums.create!(:picture => a)
-					end
+					save_attachments
 				end
 				format.html { redirect_to @restaurant, notice: '등록 완료!'}
 			else
@@ -29,9 +27,45 @@ class RestaurantsController < ApplicationController
 		end
 	end
 
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+		@albums = @restaurant.albums.all
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		respond_to do |format|
+			if @restaurant.update_attributes(restaurant_params)
+				update_attachments if params[:albums]
+				format.html { redirect_to restaurants_path, notice: '수정 완료!'}
+			else
+				format.html { render action: 'edit'}
+			end
+		end
+	end
+
+	def destroy
+		Restaurant.find(params[:id]).destroy
+		flash[:success] = "삭제 완료!"
+		redirect_to restaurants_path
+	end
+
 	private
 
 		def restaurant_params
 			params.require(:restaurant).permit(:r_name, :r_phone_number, :r_click_count, :r_like, :r_distance, :r_time, :r_code, albums_attributes: [:id, :restaurant_id, :picture])
+		end
+
+		def save_attachments
+			params[:albums]['picture'].each do |p|
+				@album = @restaurant.albums.create!(:picture => p)
+			end
+		end
+
+		def update_attachments
+			@restaurant.albums.each(&:destroy) if @restaurant.albums.present?
+			params[:albums]['picture'].each do |p|
+				@album = @restaurant.albums.create!(:picture => p)
+			end
 		end
 end
