2f505eca0761471ec5eec3692d272e86fd006216
JM <limjungmok@gmail.com>
2015-08-21 17:23:43 +0900

    Comment edit update destroy complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,38 @@ class CommentsController < ApplicationController
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@comment = @restaurant.comments.create(comment_params)
 		if @comment.save
-			redirect_to :back
+			respond_to do |format|
+				format.js
+				format.html{ redirect_to @restaurant }
+			end
 		else
 			redirect_to root_path
 		end
 	end
 
 	def edit
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@comment = @restaurant.comments.find_by(restaurant_id: @restaurant.id)
 	end
 
 	def update
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@comment = @restaurant.comments.find_by(restaurant_id: @restaurant.id)
+		if @comment.update_attributes(comment_params)
+			redirect_to @restaurant
+		else
+			redirect_to root_path
+		end
 	end
 
 	def destroy
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@comment = @restaurant.comments.find_by(restaurant_id: @restaurant.id).destroy
+
+		respond_to do |format|
+				format.js
+				format.html{ redirect_to @restaurant }
+		end
 	end
 
 
