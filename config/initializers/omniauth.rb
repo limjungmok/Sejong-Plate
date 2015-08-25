6135c88d38515c18127d7f18454ed85d90cebae3
coco <jws1784@gmail.com>
2015-08-25 21:24:25 +0900

    Ajax test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   
+  provider :facebook, '1722487254645787', '8762f3431beb7f0b1ca5e71e79b507bb', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+
 end
