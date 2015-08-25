1edb78da32aaf24c00118501ee106a0654849eba
JM <limjungmok@gmail.com>
2015-08-25 23:52:03 +0900

    home changed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
 	@restaurants = Restaurant.all
 	@restaurant_ranks = Restaurant.order("r_like DESC")
+	@random_restaurants = Restaurant.all.shuffle
   end
 
   def about
