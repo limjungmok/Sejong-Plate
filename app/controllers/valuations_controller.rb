ceaf18bfdd2d207e6f4196f33dd99226192db907
JM <limjungmok@gmail.com>
2015-08-29 00:08:50 +0900

    합체

--- app/controllers/valuations_controller.rb
+++ app/controllers/valuations_controller.rb
@@ -3,18 +3,6 @@ class ValuationsController < ApplicationController
 	def create
 
 		@restaurant = Restaurant.find(params[:restaurant_id])
-		@check_user = @restaurant.valuations.find_by(:facebook_user_id => current_user.id)
-		
-		if @check_user.recommend == false
-			@check_user.set_recommend_valuation(true)
-      		@restaurant.increment! :r_like
-		elsif @check_user.recommend == true
-			@check_user.set_recommend_valuation(false)
-			@restaurant.decrement! :r_like
-		end
-
-		@check_user.save
-
 		@test = @restaurant.valuations.where(:restaurant_id => @restaurant.id).where(:facebook_user_id => current_user.id)
 		if @test
 			if @test.first.recommend == true
