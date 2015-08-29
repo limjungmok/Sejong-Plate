5a2c467d112135e52834fcfdccdc4251a61b7701
coco <jws1784@gmail.com>
2015-08-30 02:34:56 +0900

    commit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,17 +6,11 @@ class RestaurantsController < ApplicationController
 		@comments = @restaurant.comments.all
 	end
 
-	def like
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.increment! :r_like
-	end
-
-	def unlike
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.decrement! :r_like
-	end
-
 	def index
+		if logged_in? && current_user.nickname =="세종대학생"
+			redirect_to edit_facebook_user_path(:id => current_user.id)
+		end   
+		
 		@restaurants= Restaurant.paginate(page: params[:page], :per_page => 5 ).all
 		@restaurant_ranks = Restaurant.order("r_like DESC")
 	end
@@ -24,10 +18,14 @@ class RestaurantsController < ApplicationController
 	def new
 		@restaurant = Restaurant.new
 		@album = @restaurant.albums.build
+		@admin_info_user = FacebookUser.all
+		@admin_info_comment = Comment.all
 	end
 
 	def create
+
 		@restaurant = Restaurant.new(restaurant_params)
+
 		respond_to do |format|
 			if @restaurant.save
 				unless params[:albums].nil?
@@ -65,20 +63,20 @@ class RestaurantsController < ApplicationController
 
 	private
 
-		def restaurant_params
-			params.require(:restaurant).permit(:r_name, :r_phone_number, :r_like, :r_distance, :r_time, :r_code, albums_attributes: [:id, :restaurant_id, :picture])
-		end
+	def restaurant_params
+		params.require(:restaurant).permit(:r_name, :r_phone_number, :r_like, :r_distance_door, :r_distance, :r_time, :r_code, :r_latitude, :r_longtitude, albums_attributes: [:id, :restaurant_id, :picture])
+	end
 
-		def save_attachments
-			params[:albums]['picture'].each do |p|
-				@album = @restaurant.albums.create!(:picture => p)
-			end
+	def save_attachments
+		params[:albums]['picture'].each do |p|
+			@album = @restaurant.albums.create!(:picture => p)
 		end
+	end
 
-		def update_attachments
-			@restaurant.albums.each(&:destroy) if @restaurant.albums.present?
-			params[:albums]['picture'].each do |p|
-				@album = @restaurant.albums.create!(:picture => p)
-			end
+	def update_attachments
+		@restaurant.albums.each(&:destroy) if @restaurant.albums.present?
+		params[:albums]['picture'].each do |p|
+			@album = @restaurant.albums.create!(:picture => p)
 		end
+	end
 end
